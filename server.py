ab7bbe4be6bdc8ef4b155794b26adacd37ad0213
Francososa <franky.sosa@gmail.com>
2019-05-07 11:35:07 -0600

    Implement predict endpoint

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from sklearn.externals import joblib
+import traceback
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -8,5 +12,33 @@ def hello():
     return "Machine learning model API."
 
 
+@app.route("/predict", methods=["POST"])
+def predict():
+    if lr:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(lr.predict(query))
+
+            return jsonify({"prediction": str(prediction)})
+
+        except:
+            return jsonify({"trace": traceback.format_exc()})
+
+    else:
+        print("Train the model first")
+        return "No model to use"
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    try:
+        port = int(sys.argv[1])
+    except:
+        port = 12345
+
+    lr = joblib.load("model.pkl")
+    model_columns = joblib.load("model_columns.pkl")
+    app.run(port=port, debug=True)
